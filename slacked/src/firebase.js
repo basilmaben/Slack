2d7712571fe351b67e39854cccc39d8df4638bd4
basilmaben <basilmaben.bm@gmail.com>
2021-02-22 21:22:55 +0530

    config added

---
+++ slacked/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDu_SCleq02mUYPW3cLbWvkOmrMVevFwPY",
+  authDomain: "slack-clone-a5467.firebaseapp.com",
+  projectId: "slack-clone-a5467",
+  storageBucket: "slack-clone-a5467.appspot.com",
+  messagingSenderId: "212268689634",
+  appId: "1:212268689634:web:438281520ad05d9dec16bb",
+};
