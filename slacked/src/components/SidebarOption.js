c445f465f14dce97d910c92d64b00dc462c19c71
basilmaben <basilmaben.bm@gmail.com>
2021-03-06 20:15:41 +0530

    Icons imported

---
+++ slacked/src/components/SidebarOption.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components"
+function SidebarOption({Icon, title}) {
+    return (
+        <SidebarOptionContainer>
+            
+        </SidebarOptionContainer>
+    )
+}
+
+export default SidebarOption;
+
+const SidebarOptionContainer = styled.div`
+
+`;
