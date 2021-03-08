cc9e0492b6713e1961e1614173be28a5d89aa4d2
basilmaben <basilmaben.bm@gmail.com>
2021-03-08 18:23:02 +0530

    setup firebase w google auth

--- slacked/src/firebase.js
+++ slacked/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyDu_SCleq02mUYPW3cLbWvkOmrMVevFwPY",
   authDomain: "slack-clone-a5467.firebaseapp.com",
@@ -6,3 +8,12 @@ const firebaseConfig = {
   messagingSenderId: "212268689634",
   appId: "1:212268689634:web:438281520ad05d9dec16bb",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+
+export {auth , provider, db };
