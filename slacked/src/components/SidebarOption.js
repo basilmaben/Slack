3e150dff80be3cd342a9cde828adf308e9e03108
basilmaben <basilmaben.bm@gmail.com>
2021-03-09 15:51:52 +0530

    channel preview added

--- slacked/src/components/SidebarOption.js
+++ slacked/src/components/SidebarOption.js
@@ -1,34 +1,44 @@
 import React from "react";
 import styled from "styled-components";
 import { db } from "../firebase";
+/* import { useDispatch } from "react-redux";
+import { enterRoom } from "../features/appSlice";
+ */
 
+function SidebarOption({ Icon, title, addChannelOption, id }) {
+/*   const dispatch = useDispatch(); */
 
-function SidebarOption({ Icon, title , addChannelOption, id}) {
-   
+  const addChannel = () => {
+    const channelName = prompt("Please Enter your Channel Name");
 
-    const addChannel = () => {
-        const channelName = prompt('Please Enter your Channel Name');
+    if (channelName) {
+      db.collection("rooms").add({
+        name: channelName,
+      });
+    }
+  };
 
-        if (channelName){
-            db.collection("rooms").add({
-                name: channelName,
-            });
-        }
-    };
+  const selectChannel = () => {
+    /* if (id) {
+      dispatch(
+        enterRoom({
+          roomId: id
+        })
+      );
+    } */
+  };
 
-    const selectChannel = () => {}    
-    
   return (
     <SidebarOptionContainer
-        onClick = {addChannelOption ? addChannel : selectChannel}
+      onClick={addChannelOption ? addChannel : selectChannel}
     >
       {Icon && <Icon fontSize="small" style={{ padding: 10 }} />}
-      {Icon ?(
-          <h3>{title}</h3>
-      ): (
-          <SidebarOptionChannel>
-              <span>#</span> {title}
-          </SidebarOptionChannel>
+      {Icon ? (
+        <h3>{title}</h3>
+      ) : (
+        <SidebarOptionChannel>
+          <span>#</span> {title}
+        </SidebarOptionChannel>
       )}
     </SidebarOptionContainer>
   );
@@ -37,29 +47,27 @@ function SidebarOption({ Icon, title , addChannelOption, id}) {
 export default SidebarOption;
 
 const SidebarOptionContainer = styled.div`
-display: flex;
-font-size: 12px;
-align-items: center;
-padding-left: 2px;
-cursor: pointer;
+  display: flex;
+  font-size: 12px;
+  align-items: center;
+  padding-left: 2px;
+  cursor: pointer;
 
-:hover{
+  :hover {
     opacity: 0.9;
     background-color: #340e36;
-}
+  }
 
-> h3{
+  > h3 {
     font-weight: 500;
+  }
 
-}
-
-> h3 > span{
+  > h3 > span {
     padding: 15px;
-}
+  }
 `;
 
-
 const SidebarOptionChannel = styled.h3`
-padding : 10px 0;
-font-weight: 300;
+  padding: 10px 0;
+  font-weight: 300;
 `;
