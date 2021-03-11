1e4c242a06adc266c36eb0a1b2175c381b2907ab
basilmaben <basilmaben.bm@gmail.com>
2021-03-11 16:47:16 +0530

    adding messages to db

--- src/components/ChatInput.js
+++ src/components/ChatInput.js
@@ -1,26 +1,67 @@
-import { Button } from '@material-ui/core';
-import React from 'react'
-import styled from 'styled-components'
+import { Button } from "@material-ui/core";
+import React, { useState } from "react";
+import styled from "styled-components";
+import { db } from "../firebase";
+import firebase from "firebase";
 
-function ChatInput(channelName, channelId) {
+function ChatInput({ channelName, channelId }) {
+  const [input, setInput] = useState("");
 
-    const sendMessage = (e) => {
-        e.preventDefault();
+  const sendMessage = (e) => {
+    e.preventDefault();
+
+    console.log(channelId);
+
+    if (!channelId) {
+      return false;
     }
 
-    return (
+    db.collection("rooms").doc(channelId).collection("messages").add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: "Basil Maben",
+    });
 
-        <ChatInputContainer>
-            <form>
-                <input placeholder = {`Message #Room`}/>
-                <Button hidden type = 'submit' onClick = {sendMessage}>
-                    Send
-                </Button>
-            </form>
-        </ChatInputContainer>
-    )
+    setInput("");
+  };
+
+  return (
+    <ChatInputContainer>
+      <form>
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder={`Message #${channelName}`}
+        />
+        <Button hidden type="submit" onClick={sendMessage}>
+          Send
+        </Button>
+      </form>
+    </ChatInputContainer>
+  );
 }
 
-export default ChatInput
+export default ChatInput;
 
-const ChatInputContainer = styled.div``;
+const ChatInputContainer = styled.div`
+  border-radius: 20px;
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    bottom: 30px;
+    width: 60%;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > button {
+    display: none !important;
+  }
+`;
