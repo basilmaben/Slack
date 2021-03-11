01855d5287ecd2577f143a60e68904fc9bc64935
basilmaben <basilmaben.bm@gmail.com>
2021-03-11 16:46:50 +0530

    adding room details chatbox w string interpolation

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,91 +1,102 @@
-import  InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
-import StarBorderOutlinedIcon from '@material-ui/icons/StarBorderOutlined'
-import React from 'react'
-import { useSelector } from 'react-redux'
-import styled from 'styled-components'
-import { selectRoomId } from '../features/appSlice'
-import ChatInput from './ChatInput'
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
+import React from "react";
+import { useCollection, useDocument } from "react-firebase-hooks/firestore";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { selectRoomId } from "../features/appSlice";
+import { db } from "../firebase";
+import ChatInput from "./ChatInput";
 
 function Chat() {
+  const roomId = useSelector(selectRoomId);
+  const [roomDetails] = useDocument(
+    roomId && db.collection("rooms").doc(roomId)
+  );
 
-    const roomId = useSelector(selectRoomId);
-    return (
-        <ChatContainer>
-            <>
-            <Header>
+  const [roomMessages] = useCollection(
+    roomId &&
+      db
+        .collection("rooms")
+        .doc(roomId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
 
-                <HeaderLeft>
-                    <h4><strong>#Room-name</strong></h4>
-                    <StarBorderOutlinedIcon/>
-                </HeaderLeft>
-                
-                <HeaderRight>
-                    <p>
-                        <InfoOutlinedIcon/> Details
-                    </p>
-                </HeaderRight>
-  
-            </Header>
+  console.log(roomDetails?.data());
+  console.log(roomMessages);
 
-            <ChatMessages>
-                {/* list of messages */}
-            </ChatMessages>
+  return (
+    <ChatContainer>
+      <>
+        <Header>
+          <HeaderLeft>
+            <h4>
+              <strong>#Room-name</strong>
+            </h4>
+            <StarBorderOutlinedIcon />
+          </HeaderLeft>
 
-            <ChatInput
-                //ChannelName
-                channelId = {roomId}
-            />
+          <HeaderRight>
+            <p>
+              <InfoOutlinedIcon /> Details
+            </p>
+          </HeaderRight>
+        </Header>
 
-            </>
-        </ChatContainer>
-    )
+        <ChatMessages>{/* list of messages */}</ChatMessages>
+
+        <ChatInput
+          channelName = {roomDetails?.data().name}
+          channelId={roomId}
+        />
+      </>
+    </ChatContainer>
+  );
 }
 
-export default Chat
+export default Chat;
 
 const Header = styled.div`
-display: flex;
-justify-content: space-between;
-padding: 20px;
-border-bottom: 1px solid lightgrey
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+  border-bottom: 1px solid lightgrey;
 `;
 
 const ChatMessages = styled.div``;
 
-
 const HeaderLeft = styled.div`
-display: flex;
-align-items: center;
-> h4{
+  display: flex;
+  align-items: center;
+  > h4 {
     display: flex;
     text-transform: lowercase;
     margin-right: 10px;
+  }
 
-}
-
-> h4 > .MuiSvgIcon-root{
+  > h4 > .MuiSvgIcon-root {
     margin-left: 10px;
     font-size: 18px;
-
-}
+  }
 `;
 
 const HeaderRight = styled.div`
->p{
+  > p {
     display: flex;
-align-items: center;
-font-size: 14px;
-}
+    align-items: center;
+    font-size: 14px;
+  }
 
-> p > .MuiSvgIcon-root{
+  > p > .MuiSvgIcon-root {
     margin-right: 5px !important;
     font-size: 16px;
-
-}`;
+  }
+`;
 
 const ChatContainer = styled.div`
-flex: 0.7;
-flex-grow: 1;
-overflow-y: scroll;
-margin-top: 60px;
+  flex: 0.7;
+  flex-grow: 1;
+  overflow-y: scroll;
+  margin-top: 60px;
 `;
