7342f801c48d4228399beeb31a9a5114468e2728
basilmaben <basilmaben.bm@gmail.com>
2021-03-11 20:38:33 +0530

    sign out, avatar added from db

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,35 @@
 import React from "react";
 import styled from "styled-components";
-import {Avatar} from "@material-ui/core";
+import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
-import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../firebase";
 
 function Header() {
+  const [user] = useAuthState(auth);
+
+
   return (
     <HeaderCointainer>
       {/* Header Left */}
       <HeaderLeft>
-        <HeaderAvatar/>
-        <AccessTimeIcon/>    
+        <HeaderAvatar
+          onClick={() => auth.signOut()}
+          alt={user?.displayName}
+          src={user?.photoURL}
+        />
+        <AccessTimeIcon />
       </HeaderLeft>
       {/* Header Search */}
       <HeaderSearch>
-        <SearchIcon/>
-        <input placeholder = "Search"/>
+        <SearchIcon />
+        <input placeholder="Search" />
       </HeaderSearch>
       {/* Header Right*/}
       <HeaderRight>
-      <HelpOutlineIcon/>
+        <HelpOutlineIcon />
       </HeaderRight>
     </HeaderCointainer>
   );
@@ -29,52 +38,48 @@ function Header() {
 export default Header;
 
 const HeaderSearch = styled.div`
-flex: 0.4;
-opacity: 1;
-border-radius: 6px;
-background-color: #421f44;
-text-align: center;
-display: flex;
-padding: 0 50px;
-color: gray;
-border: 1px gray solid;   
+  flex: 0.4;
+  opacity: 1;
+  border-radius: 6px;
+  background-color: #421f44;
+  text-align: center;
+  display: flex;
+  padding: 0 50px;
+  color: gray;
+  border: 1px gray solid;
 
-> input{
-  background-color: transparent;
-  border: none;
-  text-align:center;
-  min-width: 30vw;
-  outline: 0;
-  color: white;
-
-} 
-
+  > input {
+    background-color: transparent;
+    border: none;
+    text-align: center;
+    min-width: 30vw;
+    outline: 0;
+    color: white;
+  }
 `;
 
 const HeaderCointainer = styled.div`
-display:flex;
+  display: flex;
 
-position: fixed;
-width: 100%;
-align-items: center;
-justify-content: space-between;
-padding: 10px 0;
-background-color:  var(--slackcolor);
-color: white;
-
+  position: fixed;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10px 0;
+  background-color: var(--slackcolor);
+  color: white;
 `;
 
 const HeaderLeft = styled.div`
-flex: 0.3;
-display: flex;
-align-items: center;
-margin-left: 20px;
+  flex: 0.3;
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
 
-> .MuiSvgIcon-root {
-  margin-left: auto;
-  margin-right: 30px;
-}
-
+  > .MuiSvgIcon-root {
+    margin-left: auto;
+    margin-right: 30px;
+  }
 `;
 
 const HeaderRight = styled.div`
@@ -82,18 +87,16 @@ const HeaderRight = styled.div`
   display: flex;
   align-items: flex-end;
 
-  > .MuiSvgIcon-root{
+  > .MuiSvgIcon-root {
     margin-left: auto;
     margin-right: 20px;
   }
 `;
 
 const HeaderAvatar = styled(Avatar)`
-cursor: pointer;
+  cursor: pointer;
 
-
-:hover{
-  opacity: 0.8;
-}
-
+  :hover {
+    opacity: 0.8;
+  }
 `;
